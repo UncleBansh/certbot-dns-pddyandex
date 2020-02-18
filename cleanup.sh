ea536775fe9fd4c59a270c801acf35427e9de634
root <root@localhost.localdomain>
2020-02-18 18:17:40 +0300

    fix 3-4th level domain

---
+++ cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+_dir="$(dirname "$0")"
+
+source "$_dir/config.sh"
+
+if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID ]; then
+        RECORD_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID)
+        rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
+fi
+
+DOMAIN=`echo $CERTBOT_DOMAIN | rev | cut -d"." -f "1,2" | rev`
+
+# Remove the challenge TXT record from the zone
+if [ -n "${RECORD_ID}" ]; then
+	
+	RESULT=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/del" \
+     -H "PddToken: $API_KEY" \
+     -d "domain=$DOMAIN&record_id=$RECORD_ID" \
+	 | python -c "import sys,json;print(json.load(sys.stdin)['success'])")
+	
+	echo $RESULT 
+fi
